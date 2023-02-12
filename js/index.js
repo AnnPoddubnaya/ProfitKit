97e6db759c3b35943ac9582c66a4d9d07ad77e7d
Анна Поддубная <pod.dubnaya.anna1990@gmail>
2023-02-12 11:03:23 +0300

    add icons

--- js/index.js
+++ js/index.js
@@ -30,8 +30,10 @@ tabItem.forEach(item => {
 function selectTabItem() {
     tabItem.forEach(item => {
         item.classList.remove('active');
+        item.classList.add('no-active')
     })
     this.classList.add('active');
+    this.classList.remove('no-active')
     tabActive = this.getAttribute('data-tab');
     selectTabContent(tabActive)
 }
@@ -47,8 +49,53 @@ function selectTabContent(tabActive) {
     })
 
 }
+//swiper
+const blockSlide = document.querySelector('.swiper__mob');
+blockSlide.addEventListener('touchstart', touchStart, false);
+blockSlide.addEventListener('touchmove', touchMove, false);
 
 
+
+let x1 = null;
+let y1 = null;
+
+function touchStart(event) {
+    const firstTouch = event.touches[0];
+
+    x1 = firstTouch.clientX;
+    console.log(x1)
+    y1 = firstTouch.clientY;
+    console.log(x1)
+}
+
+function touchMove(event) {
+    if (!x1 || !y1) {
+        return false
+    }
+    let x2 = event.touches[0].clientX;
+    console.log(x2);
+    let y2 = event.touches[0].clientY;
+    console.log(y2);
+
+    let xDif = x2 - x1;
+    let yDif = y2 - y1;
+
+    if (Math.abs(xDif) > Mach.abs(yDif)) {
+        if (xDif > 0) {
+            console.log('right')
+        } else {
+            console.log('left')
+        }
+    } else {
+        if (yDif > 0) {
+            console.log('down')
+        } else {
+            console.log('up')
+        }
+    }
+    x1 = null;
+    y1 = null;
+}
 
 
 //слайдер
